33aee5fdff7b131cfbdb93dc719d6fd0954ac0aa
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-02 17:24:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function countdown(callback) {
   window.setTimeout(callback, 2000)
 }
 
-function createMultiplier() {
-
+function createMultiplier(multiplierValue) {
+  return (n) => {multiplierValue * n}
 }
 
 function multiplier() {
